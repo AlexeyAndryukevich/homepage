8ad5a7b05919569a4eb66c05f3b7fda54325b46d
AlexeyAndryukevich <AlexeyAndryukevich@gmail.com>
2019-05-14 11:37:00 +0300

    Add, almost, colorfull feature

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 document.getElementById("french-song").addEventListener("click", function() {
     this.childNodes[3].play();
 });
+
+function switchToColor(r,g,b) {
+    try {
+        document.querySelectorAll(".page").forEach(function(page) {
+            page.style.borderColor = "rgb("+r+", "+g+", "+b+")";
+        });
+        document.querySelectorAll("main").forEach(function(main) {
+            main.style.backgroundColor = "rgba("+r+", "+g+", "+b+", 0.7)";
+        });
+    } catch (error) {}
+}
+
+function setRandomColor(){
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    var elem = document.getElementById("color-switcher");
+    switchToColor.bind(elem, r,g,b);
+}
+
+function setRandomColor(){
+    return Math.floor(Math.random() * 255);
+   
+}
+
+//хотел сделать по маусин, но не успел
+//document.getElementById("color-switcher").addEventListener("click", setRandomColor());
+//document.getElementById("color-switcher").addEventListener("click", switchToColor.bind(this, 15,15,15));
+document.getElementById("color-switcher").addEventListener("click", switchToColor.bind(this, setRandomColor(),setRandomColor(),setRandomColor()));
+//попал в замыкание, но не успею переделать
+//Сори за мусор, решил оставить попытки
