4eedf4e6f1860914896648ffa34ec5389e413799
AlexeyAndryukevich <AlexeyAndryukevich@gmail.com>
2019-05-14 15:24:08 +0300

    Fix colorfull feature

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ document.getElementById("french-song").addEventListener("click", function() {
     this.childNodes[3].play();
 });
 
-function switchToColor(r,g,b) {
+function switchToColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
     try {
         document.querySelectorAll(".page").forEach(function(page) {
             page.style.borderColor = "rgb("+r+", "+g+", "+b+")";
@@ -13,22 +16,5 @@ function switchToColor(r,g,b) {
     } catch (error) {}
 }
 
-function setRandomColor(){
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
-    var elem = document.getElementById("color-switcher");
-    switchToColor.bind(elem, r,g,b);
-}
-
-function setRandomColor(){
-    return Math.floor(Math.random() * 255);
-   
-}
-
-//хотел сделать по маусин, но не успел
-//document.getElementById("color-switcher").addEventListener("click", setRandomColor());
-//document.getElementById("color-switcher").addEventListener("click", switchToColor.bind(this, 15,15,15));
-document.getElementById("color-switcher").addEventListener("click", switchToColor.bind(this, setRandomColor(),setRandomColor(),setRandomColor()));
-//попал в замыкание, но не успею переделать
-//Сори за мусор, решил оставить попытки
+document.getElementById("color-switcher").addEventListener("mouseover", switchToColor.bind(this));
+document.getElementById("color-switcher").addEventListener("touchstart", switchToColor.bind(this));
